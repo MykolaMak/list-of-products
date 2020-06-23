55753fa04a82126e96fc3c0cd8c7be874b9a47bf
MykolaMak <makolamak@gmail.com>
2020-06-23 14:46:27 +0300

    Updates

---
+++ precache-manifest.2ae7404c1f098b3dd872cb7bb916f1ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a965f27f7c6bfb8234cf7810452f8470",
+    "url": "/list-of-products/index.html"
+  },
+  {
+    "revision": "d8594d83c86b0f3da38b",
+    "url": "/list-of-products/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b508c9be1619a75cfb0",
+    "url": "/list-of-products/static/js/2.60a30da6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/list-of-products/static/js/2.60a30da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8594d83c86b0f3da38b",
+    "url": "/list-of-products/static/js/main.39aa8c9c.chunk.js"
+  },
+  {
+    "revision": "bd371ed3d25e7b2df03a",
+    "url": "/list-of-products/static/js/runtime-main.1783fd0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/list-of-products/static/media/logo.5d5d9eef.svg"
+  }
+]);
